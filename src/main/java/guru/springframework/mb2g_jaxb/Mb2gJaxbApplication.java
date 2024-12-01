177d0d6f30f2ca41433733148e4f18069127bed1
German Fica <germafica@gmail.com>
2024-12-01 00:35:10 -0300

    Initial commit

---
+++ src/main/java/guru/springframework/mb2g_jaxb/Mb2gJaxbApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.mb2g_jaxb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mb2gJaxbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mb2gJaxbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/mb2g_jaxb/Mb2gJaxbApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.mb2g_jaxb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mb2gJaxbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
